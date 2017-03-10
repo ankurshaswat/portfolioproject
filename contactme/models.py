8ef38be3c56b6b0e4d45a165cad8fa617c9c211a
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2017-03-10 16:51:43 +0530

    message object name display

--- contactme/models.py
+++ contactme/models.py
@@ -5,14 +5,13 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.forms import ModelForm
 
 # Create your models here.
+@python_2_unicode_compatible
 class Message(models.Model):
     messager_name=models.CharField(max_length=30,default="What's Your name")
-    messager_email=models.CharField(max_length=50,default='Email comes here')
+    messager_email=models.EmailField(max_length=50,default='noemail@noemail.com')
     message_text = models.CharField(max_length=400,default='Hello')
     pub_date = models.DateTimeField('date published')
-    def __unicode__(self):
+    def __str__(self):
         return self.messager_name
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
