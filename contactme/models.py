3340195c75b6a784956d203aaf2cb50c827dbd28
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2017-03-10 17:01:25 +0530

    added text field

--- contactme/models.py
+++ contactme/models.py
@@ -9,7 +9,7 @@ from django.forms import ModelForm
 class Message(models.Model):
     messager_name=models.CharField(max_length=30,default="What's Your name")
     messager_email=models.EmailField(max_length=50,default='noemail@noemail.com')
-    message_text = models.CharField(max_length=400,default='Hello')
+    message_text = models.TextField(max_length=400,default='Hello')
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.messager_name
